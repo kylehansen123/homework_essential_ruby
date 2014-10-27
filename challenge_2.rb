fd55276ab0d1adb0eac50416becc62c83fdc0c47
Kyle Hansen <kylehansen123@gmail.com>
2014-10-26 22:43:02 -0500

    Week 5 Homework v2

--- challenge_2.rb
+++ challenge_2.rb
@@ -34,6 +34,8 @@ principal_amount = 200000.0
 # Your code goes here.
 # You shouldn't have to write code or change code anywhere else.
 
-# loan_quotes.each ...
+loan_quotes.each do |quote|
+  puts "The monthly payment from #{quote["bank"]} will be #{pmt("annual_rate", number_of_monthly_payments, principal_amount)}"
+end
 
 # ==============================================================
